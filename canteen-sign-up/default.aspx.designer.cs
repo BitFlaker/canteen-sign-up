925d30f412195403882776d0e9f55be821d15553
BitFlaker <68316793+BitFlaker@users.noreply.github.com>
2021-12-22 09:14:46 +0100

    base style finished
    base style finished, still needs to be reworked but beta design should be done

--- canteen-sign-up/default.aspx.designer.cs
+++ canteen-sign-up/default.aspx.designer.cs
@@ -24,15 +24,6 @@ namespace canteen_sign_up
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// lblInfo control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblInfo;
-
-        /// <summary>
         /// txtFirstname control.
         /// </summary>
         /// <remarks>
@@ -103,5 +94,14 @@ namespace canteen_sign_up
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtBic;
+
+        /// <summary>
+        /// lblInfo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblInfo;
     }
 }
