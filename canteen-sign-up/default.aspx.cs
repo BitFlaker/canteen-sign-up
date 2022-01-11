b5d7730f87786e77ef293872329ed85d330cafd1
BitFlaker <68316793+BitFlaker@users.noreply.github.com>
2022-01-11 12:06:57 +0100

    worked on pdf generation

--- canteen-sign-up/default.aspx.cs
+++ canteen-sign-up/default.aspx.cs
@@ -16,6 +16,7 @@ using QRCoder;
 using System.Drawing;
 using System.Net;
 using System.Drawing.Imaging;
+using PdfSharp.Drawing.Layout;
 
 namespace canteen_sign_up
 {
@@ -115,19 +116,73 @@ namespace canteen_sign_up
 
         protected void btnSendAndPrint_Click(object sender, EventArgs e)
         {
+            double pageIndent = 70;
+
             PdfDocument document = new PdfDocument();
             document.Info.Title = "HTLVB Mensa Registrierung";
             PdfPage page = document.AddPage();
             XGraphics gfx = XGraphics.FromPdfPage(page);
-            XFont font = new XFont("Arial", 14, XFontStyle.Regular);
+            XFont font = new XFont("Arial", 12, XFontStyle.Regular);
+            XFont fontBold = new XFont("Arial", 14, XFontStyle.Bold);
+            XFont fontBoldSmaller = new XFont("Arial", 12, XFontStyle.Bold);
+            XTextFormatter tf = new XTextFormatter(gfx);
+            double regularLineHeight = font.CellSpace * 12 / font.Metrics.UnitsPerEm;
+            double boldLineHeight = fontBold.CellSpace * 14 / fontBold.Metrics.UnitsPerEm;
 
-            gfx.DrawString("Hello, World!", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
-            
+            // Generate and draw QR-Code
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode("41742720210095;rev=0", QRCodeGenerator.ECCLevel.H);
             QRCode qrCode = new QRCode(qrCodeData);
             Bitmap qrCodeImage = qrCode.GetGraphic(4);
+            MemoryStream memoryStream = new MemoryStream();
+            qrCodeImage.Save(memoryStream, ImageFormat.Png);
+            XImage image = XImage.FromStream(memoryStream);
+            double width = image.PixelWidth * 40 / image.HorizontalResolution;
+            double height = image.PixelHeight * 40 / image.HorizontalResolution;
+            gfx.DrawImage(image, page.Width - width - 40, 185, width, height);
 
+            tf.DrawString("SEPA Lastschrift-Mandat", fontBold, XBrushes.Black, new XRect(pageIndent, 180, page.Width - 2 * pageIndent, 0), XStringFormats.TopLeft);
+            tf.DrawString("für die HTL-Ausspeisung", fontBold, XBrushes.Black, new XRect(pageIndent, 180 + boldLineHeight, page.Width - 2 * pageIndent, 0), XStringFormats.TopLeft);
+
+            double topMargin = 270;
+            double spaceHeight = 20;
+            string textBlock1 = "Ich ermächtige/ Wir ermächtigen die HTL Vöcklabruck, Zahlungen von meinem/ unserem Konto mittels SEPA-Lastschrift einzuziehen. Zugleich weise ich mein/ weisen wir unser Kreditinstitut an, die von dem Bildungszentrum HTL-Vöcklabruck auf mein/ unser Konto gezogenen SEPA-Lastschriften einzulösen.";
+            string textBlock2 = "Ich kann/ Wir können innerhalb von acht Wochen, beginnend mit dem Belastungsdatum, die Erstattung des belasteten Betrages verlangen. Es gelten dabei die mit meinem/ unserem Kreditinstitut vereinbarten Bedingungen.";
+            string textBlock3 = "Der Betrag für die bestellten Essen wird im Nachhinein für die vergangenen 4 Wochen, eingezogen.";
+            
+            string textHeadingName = "Name des Schülers:";
+            string textHeadingClass = "Klasse:";
+            string textHeadingNumber = "Schülerausweisnummer (falls bereits bekannt):";
+            string textHeadingBankAccountOwnerName = "Name des Kontoinhabers:";
+            string textHeadingBankAccountOwnerAddress = "Adresse des Kontoinhabers:";
+            string textHeadingIBAN = "IBAN:";
+            string textHeadingBIC = "BIC (nur für Auslandskonten):";
+
+            double inset = 0;
+            double space = 5;
+
+            tf.DrawString(textBlock1, font, XBrushes.Black, new XRect(pageIndent, topMargin, page.Width.Value - 2*pageIndent, page.Height - topMargin), XStringFormats.TopLeft);
+            topMargin += GetSplittedLineCount(gfx, textBlock1, font, page.Width - 2 * pageIndent) * regularLineHeight + spaceHeight;
+            tf.DrawString(textBlock2, font, XBrushes.Black, new XRect(pageIndent, topMargin, page.Width.Value - 2 * pageIndent, page.Height - topMargin), XStringFormats.TopLeft);
+            topMargin += GetSplittedLineCount(gfx, textBlock2, font, page.Width - 2 * pageIndent) * regularLineHeight + spaceHeight;
+            tf.DrawString(textBlock3, font, XBrushes.Black, new XRect(pageIndent, topMargin, page.Width.Value - 2 * pageIndent, page.Height - topMargin), XStringFormats.TopLeft);
+            topMargin += GetSplittedLineCount(gfx, textBlock3, font, page.Width - 2 * pageIndent) * regularLineHeight + spaceHeight;
+            topMargin += 15;
+            
+            tf.DrawString(textHeadingName, font, XBrushes.Black, new XRect(pageIndent, topMargin, page.Width.Value - 2 * pageIndent, page.Height - topMargin), XStringFormats.TopLeft);
+            inset = gfx.MeasureString(textHeadingName, font).Width;
+            tf.DrawString("NAME OF USER", fontBoldSmaller, XBrushes.Black, new XRect(pageIndent + inset + space, topMargin, page.Width.Value - 2 * pageIndent, page.Height - topMargin), XStringFormats.TopLeft);
+            topMargin += regularLineHeight + space;
+
+            tf.DrawString(textHeadingClass, font, XBrushes.Black, new XRect(pageIndent, topMargin, page.Width.Value - 2 * pageIndent, page.Height - topMargin), XStringFormats.TopLeft);
+            inset = gfx.MeasureString(textHeadingClass, font).Width;
+            tf.DrawString("CLASS OF USER", fontBoldSmaller, XBrushes.Black, new XRect(pageIndent + inset + space, topMargin, page.Width.Value - 2 * pageIndent, page.Height - topMargin), XStringFormats.TopLeft);
+            topMargin += regularLineHeight + space;
+
+            tf.DrawString(textHeadingNumber, font, XBrushes.Black, new XRect(pageIndent, topMargin, page.Width.Value - 2 * pageIndent, page.Height - topMargin), XStringFormats.TopLeft);
+            inset = gfx.MeasureString(textHeadingNumber, font).Width;
+            tf.DrawString("NUMBER OF USER", fontBoldSmaller, XBrushes.Black, new XRect(pageIndent + inset + space, topMargin, page.Width.Value - 2 * pageIndent, page.Height - topMargin), XStringFormats.TopLeft);
+            topMargin += regularLineHeight + space;
 
             MemoryStream memoryStream1 = new MemoryStream();
             var test = Server.MapPath("images/HTLHeader.png");
@@ -140,20 +195,36 @@ namespace canteen_sign_up
                 }
             }
             XImage image1 = XImage.FromStream(memoryStream1);
-            double width1 = image1.PixelWidth * 40 / image1.HorizontalResolution;
-            double height1 = image1.PixelHeight * 40 / image1.HorizontalResolution;
-            gfx.DrawImage(image1, 10, 0, width1, height1);
-
-            MemoryStream memoryStream = new MemoryStream();
-            qrCodeImage.Save(memoryStream, ImageFormat.Png);
-            XImage image = XImage.FromStream(memoryStream);
-            double width = image.PixelWidth * 72 / image.HorizontalResolution;
-            double height = image.PixelHeight * 72 / image.HorizontalResolution;
-            gfx.DrawImage(image, page.Width - width, 200, width, height);
+            double width1 = image1.PixelWidth * 11.5 / image1.HorizontalResolution;
+            double height1 = image1.PixelHeight * 10.5 / image1.VerticalResolution;
+            double spaceWidth = (gfx.PageSize.Width - width1) / 2;
+            gfx.DrawImage(image1, spaceWidth, 0, width1, height1);
 
             string filename = Path.GetTempPath() + "HelloWorld.pdf";
             document.Save(filename);
             Process.Start(filename);
+        }
+
+        /// <summary>
+        /// Calculate the number of soft line breaks
+        /// Copied from: https://www.py4u.net/discuss/740567
+        /// </summary>
+        private static int GetSplittedLineCount(XGraphics gfx, string content, XFont font, double maxWidth)
+        {
+            Func<string, IList<string>> listFor = val => new List<string> { val };
+            Func<string, bool> nOe = str => string.IsNullOrEmpty(str);
+            Func<string, string> sIe = str => nOe(str) ? " " : str;
+            Func<string, string, bool> canFitText = (t1, t2) => gfx.MeasureString($"{(nOe(t1) ? "" : $"{t1} ")}{sIe(t2)}", font).Width <= maxWidth;
+            Func<IList<string>, string, IList<string>> appendtoLast =
+                    (list, val) => list.Take(list.Count - 1)
+                                       .Concat(listFor($"{(nOe(list.Last()) ? "" : $"{list.Last()} ")}{sIe(val)}"))
+                                       .ToList();
+            var splitted = content.Split(' ');
+            var lines = splitted.Aggregate(listFor(""),
+                    (lfeed, next) => canFitText(lfeed.Last(), next) ? appendtoLast(lfeed, next) : lfeed.Concat(listFor(next)).ToList(),
+                    list => list.Count());
+
+            return lines;
         }
     }
 }
