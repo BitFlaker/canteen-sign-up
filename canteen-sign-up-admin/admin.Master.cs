062c72185e8d2b9679e686f5e9d499208b720b47
Sebastian <94899339+Sebbi1911@users.noreply.github.com>
2022-02-09 12:06:48 +0100

    Added status changes
    Statuses change (except pending.aspx)

--- canteen-sign-up-admin/admin.Master.cs
+++ canteen-sign-up-admin/admin.Master.cs
@@ -17,29 +17,35 @@ namespace canteen_sign_up_admin
 
         protected void btnPending_Click(object sender, EventArgs e)
         {
-            DeactivateÁllNavButtons(sender);
+            DeactivateAllNavButtons(sender);
             Response.Redirect("pending.aspx");
         }
 
         protected void btnConfirmed_Click(object sender, EventArgs e)
         {
-            DeactivateÁllNavButtons(sender);
+            DeactivateAllNavButtons(sender);
             Response.Redirect("confirmed.aspx");
         }
         
         protected void btnActive_Click(object sender, EventArgs e)
         {
-            DeactivateÁllNavButtons(sender);
+            DeactivateAllNavButtons(sender);
             Response.Redirect("active.aspx");
         }
+        
+        protected void btnDeactivated_Click(object sender, EventArgs e)
+        {
+            DeactivateAllNavButtons(sender);
+            Response.Redirect("deactivated.aspx");
+        }
 
-        private void DeactivateÁllNavButtons(object sender)
+        private void DeactivateAllNavButtons(object sender)
         {
             Button navButton = sender as Button;
             btnActive.Attributes["class"] = "sideNavButton";
             btnConfirmed.Attributes["class"] = "sideNavButton";
             btnPending.Attributes["class"] = "sideNavButton";
-            btnInactive.Attributes["class"] = "sideNavButton";
+            btnDeactivated.Attributes["class"] = "sideNavButton";
         }
 
         public HtmlForm Form { get { return form1; } }
